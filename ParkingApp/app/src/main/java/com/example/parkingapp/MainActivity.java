d3ec5ad8061bc86cb6774ae451733b2f06a54f71
nashdashin <77025122+nashdashin@users.noreply.github.com>
2023-02-19 01:57:48 -0700

    images changed

--- ParkingApp/app/src/main/java/com/example/parkingapp/MainActivity.java
+++ ParkingApp/app/src/main/java/com/example/parkingapp/MainActivity.java
@@ -28,9 +28,32 @@ public class MainActivity extends AppCompatActivity {
         Button btn = findViewById(R.id.theRefreshButton);
         displayParkingSpot = (TextView) findViewById(R.id.textView);
         ImageView imageViewEastGate = findViewById(R.id.eastGateParkade);
+        ImageView imageViewEastGateRed = findViewById(R.id.eastGateParkade_RED);
+        ImageView imageViewLotA = findViewById(R.id.parkingLotA);
+        ImageView imageViewLotARed = findViewById(R.id.parkingLotA_RED);
+        ImageView imageViewLotB = findViewById(R.id.parkingLotB);
+        ImageView imageViewLotBRed = findViewById(R.id.parkingLotB_RED);
+        ImageView imageViewLot1 = findViewById(R.id.parkingLot1);
+        ImageView imageViewLot1Red = findViewById(R.id.parkingLot1_RED);
+        ImageView imageViewLot2 = findViewById(R.id.parkingLot2);
+        ImageView imageViewLot2Red = findViewById(R.id.parkingLot2_RED);
+        ImageView imageViewLot3 = findViewById(R.id.parkingLot3);
+        ImageView imageViewLot3Red = findViewById(R.id.parkingLot3_RED);
         ImageView imageViewLot4 = findViewById(R.id.parkingLot4);
+        ImageView imageViewLot4Red = findViewById(R.id.parkingLot4_RED);
         ImageView imageViewLot5 = findViewById(R.id.parkingLot5);
-        ImageView imageViewLotB = findViewById(R.id.parkingLot6A);
+        ImageView imageViewLot5Red = findViewById(R.id.parkingLot5_RED);
+        ImageView imageViewLot6 = findViewById(R.id.parkingLot6);
+        ImageView imageViewLot6Red = findViewById(R.id.parkingLot6_RED);
+        ImageView imageViewLot6A = findViewById(R.id.parkingLot6A);
+        ImageView imageViewLot6ARed = findViewById(R.id.parkingLot6A_RED);
+        ImageView imageViewLot7 = findViewById(R.id.parkingLot7);
+        ImageView imageViewLot7Red = findViewById(R.id.parkingLot7_RED);
+        ImageView imageViewLot8 = findViewById(R.id.parkingLot8);
+        ImageView imageViewLot8Red = findViewById(R.id.parkingLot8_RED);
+        ImageView imageViewLot9 = findViewById(R.id.parkingLot9);
+        ImageView imageViewLot9Red = findViewById(R.id.parkingLot9_RED);
+
 
         Drawable drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.eastgateparkade_green);
         Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
