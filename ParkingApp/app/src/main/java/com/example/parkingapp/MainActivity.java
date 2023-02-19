f4e184b2470b77fd0ea3f8874b99f8bd6907e1da
nashdashin <77025122+nashdashin@users.noreply.github.com>
2023-02-18 18:27:58 -0700

    text view with parking spots?

--- ParkingApp/app/src/main/java/com/example/parkingapp/MainActivity.java
+++ ParkingApp/app/src/main/java/com/example/parkingapp/MainActivity.java
@@ -5,20 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView displayParkingSpot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn = findViewById(R.id.theRefreshButton);
+        displayParkingSpot = (TextView) findViewById(R.id.textView);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
+                displayParkingSpot.setText("14/20");
             }
         });
 
